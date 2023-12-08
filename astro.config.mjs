dd457d5ed9cdf1dfdac378fab05b5cf8311ddff3
Mike Ambrozaitis <mambroz27@users.noreply.github.com>
2023-12-08 16:04:46 -0500

    Astro 4.0
    - Upgraded Astro,
    - Added external link plugin for markdown
    - Updated link components with better rel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from "astro/config";
 import { astroImageTools } from "astro-imagetools";
+import rehypeExternalLinks from "rehype-external-links";
 import { remarkReadingTime } from "./integrations/remark-reading-time.mjs";
 import tailwind from "@astrojs/tailwind";
 import expressiveCode from "astro-expressive-code";
@@ -7,6 +8,7 @@ import mdx from "@astrojs/mdx";
 import svelte from "@astrojs/svelte";
 
 // TODO: ⏬ Build custom  Astro Expressive Code theme
+
 /** @type {import('astro-expressive-code').AstroExpressiveCodeOptions} */
 const expressiveCodeOptions = {
   themes: ["one-dark-pro"],
@@ -16,6 +18,14 @@ const expressiveCodeOptions = {
 export default defineConfig({
   site: "https://onceuponanipsum.dev",
   markdown: {
+    rehypePlugins: [
+      [
+        rehypeExternalLinks,
+        {
+          rel: ["external", "noopener"],
+        },
+      ],
+    ],
     remarkPlugins: [remarkReadingTime],
   },
   integrations: [
